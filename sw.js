17ffa348ecf4aedf965e57338d28437d5ef3b317
TorredeControlCSS <pedregaljuandiaz@gmail.com>
2026-08-30 04:51:02 -0500

    Update cache version to 'siguelo-portal-v4'

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    otros repos GitHub Pages y deben cargar siempre en vivo.
    ============================================================ */
 
-const CACHE = "siguelo-portal-v3";   // <-- sube este número al publicar cambios
+const CACHE = "siguelo-portal-v4";   // <-- sube este número al publicar cambios
 
 const SHELL = [
   "./index.html",
