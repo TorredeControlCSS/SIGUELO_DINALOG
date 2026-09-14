397ebc5ff2c6e22da1e00b3faa2ff4bddda9c5e3
TorredeControlCSS <pedregaljuandiaz@gmail.com>
2026-09-14 05:18:37 -0500

    Update cache version to v34

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    otros repos GitHub Pages y deben cargar siempre en vivo.
    ============================================================ */
 
-const CACHE = "siguelo-portal-v33";   // <-- sube este número al publicar cambios
+const CACHE = "siguelo-portal-v34";   // <-- sube este número al publicar cambios
 
 const SHELL = [
   "./index.html",
